9e3d38e1aa193a4b35c3a031cd7859f3da87075b
Blake Baldwin <bnbaldwin1@gmail.com>
2018-10-15 13:31:43 +1100

    Dialogue system

---
+++ UniGameDevGame/Assets/ActivateDialogue.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateDialogue : MonoBehaviour {
+
+    private GameObject player;
+    private float distance;
+    private DialogueTrigger trigger;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        trigger = gameObject.GetComponent<DialogueTrigger>();
+	}
+
+    private void Update()
+    {
+        distance = Vector2.Distance(player.transform.position, gameObject.transform.position);
+        if (distance <= 5f)
+            trigger.TriggerDialogue();
+    }
+}
